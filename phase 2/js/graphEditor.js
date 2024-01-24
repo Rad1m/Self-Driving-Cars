16aadd0fc05b594de34f6f41062dee184f4c60f3
Radim Simanek <radim@Radims-M3.local>
2024-01-24 13:51:34 +0100

    pan canvas

--- phase 2/js/graphEditor.js
+++ phase 2/js/graphEditor.js
@@ -1,6 +1,7 @@
 class GraphEditor {
-  constructor(canvas, graph) {
-    this.canvas = canvas;
+  constructor(viewport, graph) {
+    this.viewport = viewport; // this is necessary for the zoom to function properly
+    this.canvas = viewport.canvas;
     this.graph = graph;
 
     this.ctx = this.canvas.getContext("2d");
@@ -21,8 +22,12 @@ class GraphEditor {
   }
 
   #handleMouseMove(evt) {
-    this.mouse = new Point(evt.offsetX, evt.offsetY);
-    this.hovered = getNearestPoint(this.mouse, this.graph.points, 20);
+    this.mouse = this.viewport.getMouse(evt);
+    this.hovered = getNearestPoint(
+      this.mouse,
+      this.graph.points,
+      20 * this.viewport.zoom
+    );
     if (this.dragging) {
       this.selected.x = this.mouse.x;
       this.selected.y = this.mouse.y;
--- phase 2/js/viewport.js
+++ phase 2/js/viewport.js
@@ -4,8 +4,26 @@ class Viewport {
     this.ctx = canvas.getContext("2d");
 
     this.zoom = 1;
+    this.center = new Point(canvas.width / 2, canvas.height / 2);
+    this.offset = scale(this.center, -1);
+
+    //implement pan the screen function
+    this.pan = {
+      start: new Point(0, 0),
+      end: new Point(0, 0),
+      offset: new Point(0, 0),
+      active: false,
+    };
 
     this.#addEventListeners();
+  }
+
+  getMouse(evt) {
+    return new Point(evt.offsetX * this.zoom, evt.offsetY * this.zoom);
+  }
+
+  getOffset() {
+    return add(this.offset, this.pan.offset);
   }
 
   #addEventListeners() {
@@ -13,6 +31,35 @@ class Viewport {
       "mousewheel",
       this.#handleMouseWheel.bind(this)
     );
+    this.canvas.addEventListener("mousedown", this.#handleMouseDown.bind(this));
+    this.canvas.addEventListener("mousemove", this.#handleMouseMove.bind(this));
+    this.canvas.addEventListener("mouseup", this.#handleMouseUp.bind(this));
+  }
+
+  #handleMouseDown(evt) {
+    if (evt.button == 1) {
+      this.pan.start = this.getMouse(evt);
+      this.pan.active = true;
+    }
+  }
+
+  #handleMouseMove(evt) {
+    if (this.pan.active) {
+      this.pan.end = this.getMouse(evt);
+      this.pan.offset = subtract(this.pan.end, this.pan.start);
+    }
+  }
+
+  #handleMouseUp(evt) {
+    if (this.pan.active) {
+      this.offset = add(this.offset, this.pan.offset);
+      this.pan = {
+        start: new Point(0, 0),
+        end: new Point(0, 0),
+        offset: new Point(0, 0),
+        active: false,
+      };
+    }
   }
 
   #handleMouseWheel(evt) {
